2dc3bf57c3f3dee5a30b34433913f4518d5dd158
Marcella Gringeri <marcella118@gmail.com>
2018-06-25 22:20:20 -0400

    Adds buttons for high ABV beers

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let beerData = [];
+let highABVData = [];
 const beerApiEndpoint = 'https://api.punkapi.com/v2/beers';
+const highABVBeerApiEndpoint = lowestABV => `https://api.punkapi.com/v2/beers?abv_gt=${lowestABV}`;
 
 const beerList = document.getElementById("beers");
 const beerListButton = document.getElementById("beerList");
 const clearListButton = document.getElementById("clearList");
-
+const highABVButton = document.getElementById("highABV");
+const clearHighABVButton = document.getElementById("clearHighABV");
 
 function createNode(element) {
   return document.createElement(element);
 }
 
-
 function append(parent, el) {
   return parent.appendChild(el);
 }
 
-
-function creatBeerList(data, targetUl) {
+function createBeerList(data, targetUl, listType) {
   let beers = data;
   return beers.map(beer => {
     let li = createNode('li'),
@@ -29,7 +30,11 @@ function creatBeerList(data, targetUl) {
     img.style.width = '60px';
     img.style.float = 'left';
     div.innerHTML = beer.name;
-    span.innerHTML = `${beer.tagline} ${beer.description}`;
+    if (listType == 'full') {
+      span.innerHTML = `${beer.name} ${beer.tagline} ${beer.description}`;
+    } else if (listType == 'highABV') {
+      span.innerHTML = `${beer.name} ${beer.abv}`;
+    }
     append(li, img);
     append(li, div);
     append(li, span);
@@ -38,11 +43,11 @@ function creatBeerList(data, targetUl) {
   })
 }
 
-
 function getBeers() {
   if (beerData.length) {
     console.log('already have the data, just returning it')
-    return Promise.resolve(beerData);
+    let promise = Promise.resolve(beerData)
+    return promise;
   } else {
     console.log('gotta blast')
     return fetch(beerApiEndpoint)
@@ -59,19 +64,47 @@ function getBeers() {
   }
 }
 
+function getHighABVBeers(lowestABV) {
+  if (highABVData.length) {
+    console.log('already have the high ABV beers, just returning it')
+    let promise = Promise.resolve(highABVData)
+    return promise;
+  } else {
+    console.log('gotta blast')
+    return fetch(highABVBeerApiEndpoint(lowestABV))
+      .then(function(resp) {
+        return resp.json()
+      })
+      .then((data) => {
+        highABVData = data;
+        return highABVData;
+      })
+      .catch(error => {
+        console.log((error));
+      });
+  }
+}
 
 function clearBeers() {
   beerList.innerHTML = "";
 }
 
-
 beerListButton.addEventListener('click', () => {
   const beerPromise = getBeers();
 
   beerPromise.then((data) => { 
-    creatBeerList(data, beerList);
+    createBeerList(data, beerList, 'full');
   });
 });
 
-
 clearListButton.addEventListener('click', clearBeers);
+
+highABVButton.addEventListener('click', () => {
+  const beerPromise = getHighABVBeers(7);
+
+  beerPromise.then((data) => {
+    createBeerList(data, beerList, 'highABV');
+  });
+});
+
+clearHighABVButton.addEventListener('click', clearBeers);
